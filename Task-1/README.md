21e702356359cf0e3b2cdd78c8349f497a132964
Ihor <88674695+EnotShow@users.noreply.github.com>
2023-01-12 19:04:23 +0200

    Task-1

---
+++ Task-1/README.md
@@ -0,0 +1,6 @@
+<h1>Title: Task 1: software configuration</h1>
+
+<h2>Subtask 1: Why did I choose to participate in the Dare IT Challenge?</h2>
+Hey! My name is Ihor and I'm a python dev :) I am so much happy to be here. I love coding and wanna upgrade myself at it. I expect to gain testing skills and complete an internship by applying for this course
+
+<h3>Thanx for all that you guys do for ukrainians!<h3>
---
+++ Task-1/test_cases/framework.py
@@ -0,0 +1,29 @@
+import os
+import unittest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+from utils.settings import IMPLICITLY_WAIT
+
+
+class Test(unittest.TestCase):
+
+    @classmethod
+    def setUp(self):
+        # self.driver = webdriver.Chrome()
+
+        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        self.driver.get('https://scouts-test.futbolkolektyw.pl/en')
+        self.driver.fullscreen_window()
+        self.driver.implicitly_wait(IMPLICITLY_WAIT)
+
+    @classmethod
+    def tearDown(self):
+        self.driver.quit()
+
+    def test_print_nice_words(self):
+        print("WELL DONE!!!!!!!!!")
+
+    # Element of the first task: Try to search the Internet yourself how to get rid of the error:
+    # "DeprecationWarning: executable_path has been deprecated, please pass in a Service object"
